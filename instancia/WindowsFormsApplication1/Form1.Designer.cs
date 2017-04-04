2d0cb2ce82c4a25b21df2ad6529902dadab02cc9
agustinLescano <agustin_lescano2016@hotmail.com>
2017-04-04 12:15:36 -0300

    windowsForm

---
+++ instancia/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNombre.ForeColor = System.Drawing.Color.Red;
+            this.txtNombre.Location = new System.Drawing.Point(106, 53);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(131, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Nombre.ForeColor = System.Drawing.Color.Maroon;
+            this.Nombre.Location = new System.Drawing.Point(143, 23);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(45, 13);
+            this.Nombre.TabIndex = 1;
+            this.Nombre.Text = "nombre:";
+            this.Nombre.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(24, 163);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(90, 52);
+            this.btnMostrar.TabIndex = 2;
+            this.btnMostrar.Text = "mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(384, 376);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ instancia/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string miVariable;
+            miVariable = this.txtNombre.Text;
+            MessageBox.Show("hola mundo "+miVariable);
+            this.Nombre.Text = miVariable;
+           
+
+        }
+    }
+}
